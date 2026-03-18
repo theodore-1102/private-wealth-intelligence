4e5cddb22cf1d8740a605ed0d013d42f08bc42f2
Theodore Wee <129189937+theodore-1102@users.noreply.github.com>
2026-03-18 22:47:42 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/index.html",
+        permanent: false,
+      },
+    ];
+  },
+};
 export default nextConfig;
